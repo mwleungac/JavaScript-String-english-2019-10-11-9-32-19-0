cd231c303ef796a28ff25b24a87b3ff051c6fda9
mwleungac <57056434+mwleungac@users.noreply.github.com>
2019-11-09 13:36:06 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,12 +2,37 @@
 var name = 'hello';
 // TODO write your code here...
 
+var name = 'hello';
+console.log(name.toUpperCase())
+
+
+
 
 // Complete the following procedure, capitalizing all the words in the variable sentence and printing: 'Good Afternoon, Mr Mike.'
 var sentence = 'good afternoon, mr mike.';
 //TODO write your code here...
 
+var sentence = 'good afternoon, mr mike.';
+
+function titleCase(str) {
+   var splitStr = str.toLowerCase().split(' ');
+   for (var i = 0; i < splitStr.length; i++) {
+       // You do not need to check if i is larger than splitStr length, as your for does that for you
+       // Assign it back to the array
+       splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);     
+   }
+   // Directly return the joined string
+   return splitStr.join(' '); 
+}
+
+console.log(titleCase(sentence));
+
+
+
 
 // Complete the following procedure to extract the amount of money variable, output: 20.
 var money = '￥20';
 //TODO write your code here...
+
+var money = '￥20';
+console.log(money.substring(1,2));
